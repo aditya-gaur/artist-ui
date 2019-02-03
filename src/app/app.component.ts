f2f148a32bdb7e23083e56decddcd9587b7b565e
Aditya <adityagaur1994@gmail.com>
2019-02-03 18:24:11 +0530

    created Artist page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { faShareAlt } from '@fortawesome/free-solid-svg-icons';
+
+import data from "./helper";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'artist-ui';
+  faShareAlt = faShareAlt;
+  artistData: any;
+
+  ngOnInit() {
+    this.artistData = data;
+  }
 }
